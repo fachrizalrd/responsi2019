fafa615b9dd573d067ffd365ef7b57f97a1e667d
fachrizalrd <fachrizal.deninsyah@students.amikom.ac.id>
2019-04-16 14:45:28 +0700

    first commit

---
+++ ResponsiPemrog2248/Karyawan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2248
+{
+    class Karyawan
+    {
+        public Karyawan()
+        {
+            nik = "Empty";
+            nama = "Empty";
+            gajiBulanan = 0;
+
+        }
+
+        public Karyawan (string nik,string nama,int gajiBulanan)
+        {
+            this.nik = nik;
+            this.nama = nama;
+            if (gajiBulanan < 0)
+            {
+                this.gajiBulanan = 0;
+            }
+            else
+            {
+                this.gajiBulanan = gajiBulanan;
+            }
+        }
+
+        private string nik;
+        private string nama;
+        private int gajiBulanan;
+
+        public string NIK 
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+        public string Nama {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public int GajiBulanan
+        {
+            get { return gajiBulanan; }
+            set { gajiBulanan = value; }
+        }
+    }
+}
---
+++ ResponsiPemrog2248/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2248
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Karyawan karGus = new Karyawan("21232", "Agus", 3000000);
+            Karyawan karTri = new Karyawan("25232", "Tri", 2000000);
+
+            Console.WriteLine("No\tNIK\tNama\t\tGajiBulanan\t");
+            Console.WriteLine("1.\t{0}\t{1}\t\t{2}", karGus.NIK, karGus.Nama, karGus.GajiBulanan);
+            Console.WriteLine("2.\t{0}\t{1}\t\t{2}", karTri.NIK, karTri.Nama, karTri.GajiBulanan);
+            Console.ReadKey();
+
+            karGus.GajiBulanan = karGus.GajiBulanan + (karGus.GajiBulanan * 10 / 100);
+            karTri.GajiBulanan = karTri.GajiBulanan + (karTri.GajiBulanan * 10 / 100);
+
+            Console.WriteLine("Naik Gaji cuyyyy!!! 10%");
+            Console.WriteLine("No\tNIK\tNama\t\tGajiBulanan\t");
+            Console.WriteLine("1.\t{0}\t{1}\t\t{2}", karGus.NIK, karGus.Nama, karGus.GajiBulanan);
+            Console.WriteLine("2.\t{0}\t{1}\t\t{2}", karTri.NIK, karTri.Nama, karTri.GajiBulanan);
+
+            Console.ReadKey();
+        }
+
+       
+    }
+}
